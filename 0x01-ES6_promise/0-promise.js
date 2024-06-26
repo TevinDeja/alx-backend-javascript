ba20cc1fd1590a15d4cc37afc3e16f07d65845f7
TevinDeja <tevinowena@gmail.com>
2024-06-26 09:33:32 +0000

    Keep every promise you make and only make promises you can keep

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,9 @@
+export default function getResponseFromAPI() {
+    return new Promise((resolve, reject) => {
+        // Simulate an asynchronous operation
+        setTimeout(() => {
+            resolve("Success!");
+        }, 1000);
+    });
+}
+
