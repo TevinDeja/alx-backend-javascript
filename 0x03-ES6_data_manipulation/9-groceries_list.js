189c6b647b9003171ad68c224f1c010713a5adc1
TevinDeja <tevinowena@gmail.com>
2024-07-02 14:40:30 +0000

    Map data structure

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,11 +1,10 @@
 export default function groceriesList() {
-  const groceriesMap = new Map([
+  const values = [
     ['Apples', 10],
     ['Tomatoes', 10],
     ['Pasta', 1],
     ['Rice', 1],
     ['Banana', 5],
-  ]);
-
-  return groceriesMap;
+  ];
+  return new Map(values);
 }
