0f32c534554fa7fe94ccef8589bba2414f1e4559
TevinDeja <tevinowena@gmail.com>
2024-08-20 20:29:10 +0000

    HTTP server

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,15 +1,10 @@
 /**
-
- * Displays a message in the console.
-
+ * Displays a message in the console
  * @module 0-console
-
  */
 
 function displayMessage(message) {
-
   console.log(message);
-
 }
 
 module.exports = displayMessage;
