0fe42082d845a7457d189dce435885235a32a117
TevinDeja <tevinowena@gmail.com>
2024-08-20 13:14:44 +0000

    basic javascript with Node JS

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,15 @@
+/**
+
+ * Displays a message in the console.
+
+ * @module 0-console
+
+ */
+
+function displayMessage(message) {
+
+  console.log(message);
+
+}
+
+module.exports = displayMessage;
